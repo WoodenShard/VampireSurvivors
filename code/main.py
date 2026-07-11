4fd4db095a1c6cc2ae7a36d8b9a3aa7fe49f966e
WoodenShard <298028119+WoodenShard@users.noreply.github.com>
2026-07-11 19:23:11 +0200

    added player.py with movement logic and on main the starting stage

--- code/main.py
+++ code/main.py
@@ -1,3 +1,46 @@
 from settings import *
+from player import Player
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.display = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
+        pygame.display.set_caption('Vampire Survivors')
+        self.clock = pygame.time.Clock()
+        self.running: bool = True
+
+        self.all_sprites = pygame.sprite.Group()
+
+        self.player = Player((100,200), self.all_sprites)
+
+    def run(self):
+        while self.running:
+            # dt
+            dt = self.clock.tick() / 1000
+
+            # event loop
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running: bool = False
+            
+
+            self.all_sprites.update(dt)
+
+            self.display.fill('black')   
+            self.all_sprites.draw(self.display)        
+            pygame.display.update()
+
+        pygame.quit()
+
+
+        
+if __name__ == '__main__':
+    game = Game()
+    game.run()
+ 
+
+
+
+
 
 
---
+++ code/player.py
@@ -0,0 +1,26 @@
+from typing import Any
+
+from settings import *
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos, groups):
+        super().__init__(groups)
+        self.image = pygame.image.load(join('images','player','down','0.png')).convert_alpha()
+        self.rect: pygame.FRect = self.image.get_frect(center = pos)
+
+        # movement
+        self.direction: pygame.Vector2 = pygame.Vector2(0,0)
+        self.speed : int = 300
+
+    def input(self):
+        keys = pygame.key.get_pressed()
+        self.direction.x = int(keys[pygame.K_RIGHT] or keys[pygame.K_d]) - int(keys[pygame.K_LEFT] or keys[pygame.K_a])
+        self.direction.y = int(keys[pygame.K_DOWN] or keys[pygame.K_s]) - int(keys[pygame.K_UP] or keys[pygame.K_w])
+        self.direction = self.direction.normalize() if self.direction else self.direction
+
+    def move(self,dt):
+        self.rect.center += self.direction * self.speed * dt
+
+    def update(self, dt):
+        self.input()
+        self.move(dt)
